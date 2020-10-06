7a6b15cd35e1af37a1a743559ed5b831aa2d21bf
feedoom <qqdz3750@sina.com>
2020-10-06 13:16:20 +0800

    修复 tap 不能初始化问题

--- src/app2.js
+++ src/app2.js
@@ -4,15 +4,19 @@ const $tabBar = $('#app2 .tab-bar')
 const $tabContent = $('#app2 .tab-content')
 let index1 = parseInt(localStorage.getItem('item')) || 0
 console.log(`index2${index1}`)
+
+$tabBar.children().eq(index1).addClass('selected').siblings().removeClass('selected')
+$tabContent.children().eq(index1).addClass('active').siblings().removeClass('active')
+
 $tabBar.on('click', 'li', e => {
     const $li = $(e.currentTarget);
-    const liIndex = $li.index()
-    localStorage.setItem('item', liIndex)
+    const index1 = $li.index()
+    localStorage.setItem('item', index1)
     // console.log(liIndex)
     console.log(`index${index1}`)
-    $tabBar.children().eq(liIndex).addClass('selected').siblings().removeClass('selected')
+    $tabBar.children().eq(index1).addClass('selected').siblings().removeClass('selected')
 
-    $tabContent.children().eq(liIndex).addClass('active').siblings().removeClass('active')
+    $tabContent.children().eq(index1).addClass('active').siblings().removeClass('active')
     // 找到tabContent的儿子们的第index个的css设置为display block 而兄弟们设置display none
 })
 //事件委托
